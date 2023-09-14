edb62abf6f8c0355d47595b9167dbae045022d69
Tomás Gimenez Apostol <tommygimenez55@gmail.com>
2023-09-13 21:11:06 -0300

    algo

--- src/main/java/com/equipoA/webapp/Entidades/Usuario.java
+++ src/main/java/com/equipoA/webapp/Entidades/Usuario.java
@@ -15,28 +15,29 @@ public class Usuario {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name="uuid", strategy = "uuid2")
-    private int ID;
+    private String ID;
     private String fullName;
     private String email;
     private int phone;
     private Localidad ubicacion;
 
-    public Usuario(int ID, String fullName, String email, int phone, Localidad ubicacion) {
+    public Usuario() {
+    }
+
+    public Usuario(String ID, String fullName, String email, int phone, Localidad ubicacion) {
         this.ID = ID;
         this.fullName = fullName;
         this.email = email;
         this.phone = phone;
         this.ubicacion = ubicacion;
     }
+    
 
-    public Usuario() {
-    }
-
-    public int getID() {
+    public String getID() {
         return ID;
     }
 
-    public void setID(int ID) {
+    public void setID(String ID) {
         this.ID = ID;
     }
 
